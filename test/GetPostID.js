e190440981c26d5da628b67e23468bc1d4566998
AthallahDzaki <AthallahDzaki@gmail.com>
2021-11-05 09:01:07 +0700

    Fix Undefined if PostID not defined as string

--- test/GetPostID.js
+++ test/GetPostID.js
@@ -1,6 +1,6 @@
 const API = require('../index'); // Change To 1cak if you use node_module
 
 (async() => {
-    console.log(await API.GetPostID(1971739)); // Page ID Is Optional
+    console.log(await API.GetPostID()); // Page ID Is Optional
 })()
 
